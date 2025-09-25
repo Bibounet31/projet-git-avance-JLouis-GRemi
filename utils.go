9967ef2b98c108a18d48fe20e957122c0d03fca6
jlouis <louis.jeannot@ynov.com>
2025-09-25 15:34:17 +0200

    feat(utils): Added a function PrintPolish

---
+++ utils.go
@@ -0,0 +1,11 @@
+package utils
+
+import (
+	"fmt"
+)
+
+// this function is used to make everything look smoother by separating
+// different block of print
+func printPolish() {
+	fmt.Println("================")
+}
