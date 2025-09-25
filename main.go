6f57fd2e92aef42ed7cabcacc2161b473ce1a8e8
gremi <remi.gauci@ynov.com>
2025-09-25 15:31:43 +0200

    feat(main): Added Dontcounttoten func

--- main.go
+++ main.go
@@ -2,13 +2,21 @@ package main
 
 import "fmt"
 
+// main func, calling others
 func main() {
 	fmt.Println("Hello World")
 	Counttoten()
+	Dontcounttoten()
 }
 
+// a function that count from 0 to 10
 func Counttoten() {
 	for i := 0; i < 10; i++ {
 		fmt.Println(i)
 	}
+}
+
+// a function that doesn't count from 0 to 10
+func Dontcounttoten() {
+	fmt.Println("a")
 }
